d1990d81a3d1eb558e2ed4e6ce81aded64f75953
¨afandino21¨ <al_fl95@hotmail.com>
2023-09-05 16:56:08 -0300

    DS martes
    agregamos estilos al listado de productos al pasar el cursor
    
    Co-Authored-By: Santiago Lopez <140985897+Santilo777@users.noreply.github.com>
    Co-Authored-By: Franco <113482175+francordz99@users.noreply.github.com>
    Co-Authored-By: Marcos Araujo <140938264+mrk4os@users.noreply.github.com>
    Co-Authored-By: João Lizot <141048911+joaolizot@users.noreply.github.com>

--- js/product-info.js
+++ js/product-info.js
@@ -25,6 +25,7 @@ function loadInfo() {
         <li class="list-group-item"><img src="${productInfo.images[0]}" alt="Imagen 1" class="illustrive-image"></li>
         <li class="list-group-item"><img src="${productInfo.images[1]}" alt="Imagen 2" class="illustrive-image"></li>
         <li class="list-group-item"><img src="${productInfo.images[2]}" alt="Imagen 3" class="illustrive-image"></li>
+        <li class="list-group-item"><img src="${productInfo.images[3]}" alt="Imagen 3" class="illustrive-image"></li>
         </ul>
             `
             infoContainer.appendChild(infoList);
@@ -33,4 +34,43 @@ function loadInfo() {
             console.error('Error al cargar los productos:', error);
         });
 }
+
+/*
+function loadComments() {
+    let productID = localStorage.getItem('productID');
+    const url = `https://japceibal.github.io/emercado-api/products_comments/${productID}.json`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const productInfo = data;
+            const infoContainer = document.getElementById('info-container');
+            const infoList = document.createElement('div');
+            infoList.classList.add("list-group")
+
+            infoList.innerHTML = `
+        <div>
+            <h1> ${productInfo.name} </h1>
+        </div>
+        
+        <ul class="list-group">
+            <li class="list-group-item">Precio:${productInfo.cost} ${productInfo.currency}</li>
+            <li class="list-group-item">Descripcion:${productInfo.description} </li>
+            <li class="list-group-item">Categoria:${productInfo.category} </li>
+            <li class="list-group-item">Cantidad de vendidos:${productInfo.soldCount} </li>
+        </ul>
+        <ul class="list-group list-group-horizontal">
+        <li class="list-group-item"><img src="${productInfo.images[0]}" alt="Imagen 1" class="illustrive-image"></li>
+        <li class="list-group-item"><img src="${productInfo.images[1]}" alt="Imagen 2" class="illustrive-image"></li>
+        <li class="list-group-item"><img src="${productInfo.images[2]}" alt="Imagen 3" class="illustrive-image"></li>
+        
+        </ul>
+            `
+            infoContainer.appendChild(infoList);
+        })
+        .catch(error => {
+            console.error('Error al cargar los productos:', error);
+        });
+}
+*/
 document.addEventListener('DOMContentLoaded', loadInfo);
