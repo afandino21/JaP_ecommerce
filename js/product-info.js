1205227c4bb8135715a700360e23bddf9a1a8b26
João Lizot <premtrabajo@gmail.com>
2023-09-06 14:05:27 -0300

    commit cambio optimixacion products-info y cambio estilos

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,5 @@
 function loadInfo() {
-    let productID = localStorage.getItem('productID');
+    const productID = localStorage.getItem('productID');
     const url = `https://japceibal.github.io/emercado-api/products/${productID}.json`;
 
     fetch(url)
@@ -7,59 +7,53 @@ function loadInfo() {
         .then(data => {
             const productInfo = data;
             const infoContainer = document.getElementById('info-container');
+            
             const infoList = document.createElement('div');
             infoList.classList.add("list-group");
-
+            
             infoList.innerHTML = `
-            <div>
-                <h1>${productInfo.name}</h1>
-            </div>
-
-            <ul class="list-group">
-                <li class="list-group-item">Precio: ${productInfo.cost} ${productInfo.currency}</li>
-                <li class="list-group-item">Descripción: ${productInfo.description}</li>
-                <li class="list-group-item">Categoría: ${productInfo.category}</li>
-                <li class="list-group-item">Cantidad de vendidos: ${productInfo.soldCount}</li>
-            </ul>
-
-            <div id="carouselExample" class="carousel slide" data-ride="carousel">
-
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="${productInfo.images[0]}" alt="Imagen 1" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="${productInfo.images[1]}" alt="Imagen 2" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="${productInfo.images[2]}" alt="Imagen 3" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="${productInfo.images[3]}" alt="Imagen 4" class="d-block w-100">
-                    </div>
+                <div>
+                    <h1>${productInfo.name}</h1>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Siguiente</span>
-                </a>
-            </div>
-
-            <div>
-                <ul class="list-group list-group-horizontal">
-                    <li class="list-group-item"><img src="${productInfo.images[0]}" alt="Imagen 1" class="product-image"></li>
-                    <li class="list-group-item"><img src="${productInfo.images[1]}" alt="Imagen 2" class="product-image"></li>
-                    <li class="list-group-item"><img src="${productInfo.images[2]}" alt="Imagen 3" class="product-image"></li>
-                    <li class="list-group-item"><img src="${productInfo.images[3]}" alt="Imagen 4" class="product-image"></li>
+            
+                <ul class="list-group">
+                    <li class="list-group-item">Precio: ${productInfo.cost} ${productInfo.currency}</li>
+                    <li class="list-group-item">Descripción: ${productInfo.description}</li>
+                    <li class="list-group-item">Categoría: ${productInfo.category}</li>
+                    <li class="list-group-item">Cantidad de vendidos: ${productInfo.soldCount}</li>
                 </ul>
-            </div>
-        `;
+            
+                <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        ${productInfo.images.map((image, index) => `
+                            <div class="carousel-item ${index === 0 ? 'active' : ''}">
+                                <img src="${image}" alt="Imagen ${index + 1}" class="d-block w-100">
+                            </div>
+                        `).join('')}
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Siguiente</span>
+                    </a>
+                </div>
+            
+                <div>
+                    <ul class="list-group list-group-horizontal" id="imageThumbnails">
+                        ${productInfo.images.map((image, index) => `
+                            <li class="list-group-item abajo-carrusel">
+                                <img src="${image}" alt="Imagen ${index + 1}" class="product-image cursor-active" data-target="#carouselExample" data-slide-to="${index}">
+                            </li>
+                        `).join('')}
+                    </ul>
+                </div>
+            `;
 
             infoContainer.appendChild(infoList);
-
+            
             // Inicializa el carrusel de Bootstrap después de cargar el contenido
             $('#carouselExample').carousel();
         })
@@ -68,9 +62,8 @@ function loadInfo() {
         });
 }
 
-
 function loadComments() {
-    let productID = localStorage.getItem('productID');
+    const productID = localStorage.getItem('productID');
     const url = `https://japceibal.github.io/emercado-api/products_comments/${productID}.json`;
 
     fetch(url)
@@ -78,38 +71,33 @@ function loadComments() {
         .then(data => {
             const comments = data;
             const commentContainer = document.getElementById('comment-list');
-            const infoList = document.createElement('div');
-            infoList.classList.add("list-group");
-
-            infoList.innerHTML = `
-                <div id="comment-list">
-                </div>
-            `;
-
-            const commentList = document.getElementById('comment-list');
-
-
+            const commentForm = document.getElementById('new-comment-form');
+            
+            const commentList = document.createElement('div');
+            commentList.classList.add("list-group");
+            
+            commentContainer.appendChild(commentList);
 
             comments.forEach(comment => {
                 const formattedUser = comment.user.replace(/_/g, ' ');
+
+                const starHTML = Array(5).fill('').map((_, index) => `
+                    <span class="fa fa-star ${index < comment.score ? 'checked' : 'unchecked'}"></span>
+                `).join('');
+
                 const commentItem = document.createElement('div');
                 commentItem.classList.add("list-group-item");
                 commentItem.classList.add("commits");
 
-                let starHTML = '';
-                for (let i = 0; i < comment.score; i++) {
-                    starHTML += '<span class="fa fa-star checked"></span>';
-                }
-
                 commentItem.innerHTML = `
-                <p><strong>${formattedUser}</strong> - ${comment.dateTime} - ${starHTML}</p>
+                    <p><strong>${formattedUser}</strong> - ${comment.dateTime} - ${starHTML}</p>
                     <p>${comment.description}</p>
                 `;
 
                 commentList.appendChild(commentItem);
             });
 
-            commentContainer.appendChild(infoList);
+            commentForm.style.display = 'block'; // Muestra el formulario de comentarios
         })
         .catch(error => {
             console.error('Error al cargar los comentarios:', error);
@@ -119,8 +107,8 @@ function loadComments() {
 function addComment(event) {
     event.preventDefault();
 
-    const productID = localStorage.getItem('productID');
-    const score = document.getElementById('comment-rating').value;
+    const selectedStar = document.querySelector('.star-rating input[name="rating"]:checked');
+    const score = 6 - parseInt(selectedStar.value); // Cambio aquí para invertir la puntuación
     const description = document.getElementById('comment-text').value;
 
     const now = new Date();
@@ -133,8 +121,7 @@ function addComment(event) {
     const dateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
     const newComment = {
-        product: productID,
-        score: parseInt(score),
+        score: score, // Utiliza la puntuación invertida
         description: description,
         dateTime: dateTime
     };
@@ -153,14 +140,14 @@ function addComment(event) {
 function displayComment(comment) {
     const commentList = document.getElementById('comment-list');
     const commentItem = document.createElement('div');
-    var storedValue = localStorage.getItem("username");
+    const storedValue = localStorage.getItem("username");
+    
     commentItem.classList.add("list-group-item");
     commentItem.classList.add("commits");
 
-    let starHTML = '';
-    for (let i = 0; i < comment.score; i++) {
-        starHTML += '<span class="fa fa-star checked"></span>';
-    }
+    const starHTML = Array(5).fill('').map((_, index) => `
+        <span class="fa fa-star ${index < comment.score ? 'checked' : 'unchecked'}"></span>
+    `).join('');
 
     commentItem.innerHTML = `
         <p><strong>${storedValue}</strong> - ${comment.dateTime} - ${starHTML}</p>
@@ -177,5 +164,3 @@ document.addEventListener('DOMContentLoaded', () => {
     const commentForm = document.getElementById('new-comment-form');
     commentForm.addEventListener('submit', addComment);
 });
-
-
--- js/products.js
+++ js/products.js
@@ -105,19 +105,20 @@ function applyFiltersAndSort(products) {
     productCard.classList.add('product-card');
 
     productCard.innerHTML = `
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img src="${product.image}" alt="${product.name}" class="img-fluid product-image">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">${product.name}</h5>
-            <p class="card-text">${product.description}</p>
-            <p class="card-text">${product.cost} ${product.currency}</p>
-            <p class="card-text">${product.soldCount} vendidos</p>
-          </div>
-        </div>
-      </div>
+                <div id="tarjetasProducts" class="row cursor-active">
+                    <div class="col-3">
+                        <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${product.name}</h4>
+                            <p class="text-muted">${product.cost} ${product.currency}</p>
+                        </div>
+                        <p class="card-text">${product.soldCount} vendidos</p>
+                        <p class="mb-1">${product.description}</p>
+                    </div>
+                </div>
+            </div>
     `;
 
     // Agrega un evento de clic para redirigir al usuario y guardar la ID del producto
